1691c8bd59cb152af3a716d652596cf7872761c6
pr0tem <39151131+pr0tem@users.noreply.github.com>
2020-09-29 00:40:26 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,13 @@
+import socket
+
+port=13000
+
+s=socket.socket()
+s.connect(('127.0.0.1', port)) 
+
+while(True):
+	x = input()
+	s.send(str.encode(x))
+
+#print(s.recb(1024))
+#print(str.encode(input('da')))
