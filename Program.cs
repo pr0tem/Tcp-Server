ddd68460a53fc02c76a68d5ec0e78205d6da69a5
pr0tem <39151131+pr0tem@users.noreply.github.com>
2020-09-29 00:37:42 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Net;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+
+        private static void Main()
+        {
+            //import classes
+            ConnectionManeger connection = new ConnectionManeger();
+            ThreadManeger thread = new ThreadManeger();
+            
+            //creates lists for sockets and threads
+            List<Socket> socketArr = new List<Socket>();
+            List<Thread> threadArr = new List<Thread>();
+            
+            //start the server and start the cleaner thread 
+            TcpListener server = connection.StartServer();
+            Thread clean = new Thread(() => thread.Cleaner(socketArr, threadArr));
+            clean.Start();
+
+            while (true)
+            {
+                //create a socket bound to a thread and add it to te list if a connection is pending
+                if (server.Pending())
+                {
+                    Socket tempSocket = server.AcceptSocket();
+                    socketArr.Add(tempSocket);
+                    Thread tempThread = new Thread(() => thread.ThreadReadBytes(tempSocket));
+                    threadArr.Add(tempThread);
+                    tempThread.Start();
+                }
+            }
+        }
+    }
+}
