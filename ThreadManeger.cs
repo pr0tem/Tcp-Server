d6a262dad59959daca2b4eca56a23b8ff5b47d80
pr0tem <39151131+pr0tem@users.noreply.github.com>
2020-10-20 13:26:57 +0300

    Update ThreadManeger.cs

--- ThreadManeger.cs
+++ ThreadManeger.cs
@@ -8,35 +8,7 @@ using System.Security.Cryptography;
 namespace ConsoleApp1
 {
     class ThreadManeger
-    {
-        // i robot of threads, clean the socket/thread lists
-        public void Cleaner(List<Socket> s, List<Thread> t)
-        {
-            while (true)
-            {
-                try
-                {
-                    for (int i = 0; i < s.Count; i++)
-                    {
-                        if (s[i].Connected == null)
-                        {
-                            Thread.Sleep(5);
-                        }
-                        else if (s[i].Connected == false)
-                        {
-                            s[i].Close();
-                            s.RemoveAt(i);
-                            t[i].Interrupt();
-                            t.RemoveAt(i);
-                        }
-                    }
-                }
-                catch (Exception)
-                {
-
-                }
-            } 
-        }
+    }
         
         //A thread task that read input from client
         public void ThreadReadBytes(Socket s)
